1420943b2d3879205716fe68aca16f0b6f120fd3
Edward J. Yoon <edwardyoon@apache.org>
2019-03-13 15:30:18 +0900

    Update RedisSubscriber.scala

--- src/main/scala/RedisSubscriber.scala
+++ src/main/scala/RedisSubscriber.scala
@@ -5,8 +5,48 @@ import com.redis._
 object RedisSubscriber {
   case class Subscribe(channels: Seq[String])
   case class Unsubscribe(channels: Seq[String])
-  case class UnsubscribeAll()
   case class Published(channel: String, data: String)
+}
+
+class RedisSubscriberActor(redisClient: RedisClient) extends Actor with ActorLogging {
+
+  import RedisSubscriber._
+
+  var state = SubscriptionState(subscriber = Map.empty, subscribed = Map.empty)
+
+  override def receive: Receive = {
+    // from other actor
+    case Subscribe(channels) =>
+      state = state.onSubscribe(sender, channels: _*)
+      val me = self
+      redisClient.subscribe(channels.head, channels.tail: _*) {
+        msg: PubSubMessage =>
+          // from com.redis.PubSub.Consumer thread
+          me ! msg
+      }
+
+    case Unsubscribe(channels) =>
+      val oldState = state
+      state = state.onUnsubscribe(sender, channels: _*)
+      val toUnsubscribe = channels.filter(
+        c => oldState.numSubscriber(c) > 0 && state.numSubscriber(c) == 0
+      )
+      if (toUnsubscribe.nonEmpty) {
+        redisClient.unsubscribe(toUnsubscribe.head, toUnsubscribe.tail: _*)
+      }
+
+    // from com.redis
+    case S(channel, count) =>
+      log.info("subscribed to {} / subscribing to {}", channel, count)
+
+    case U(channel, count) =>
+      log.info("unsubscribed to {} / subscribing to {}", channel, count)
+
+    case M(channel, data) =>
+      log.info("message has arrived from redis.")
+
+    // NOT handling com.redis.redisclient.E
+  }
 }
 
 object RedisSubscriberActor {
@@ -40,71 +80,9 @@ object RedisSubscriberActor {
       SubscriptionState(subscriber = newSubscriber, subscribed = newSubscribed)
     }
 
-    def onUnsubscribeAll(actor: ActorRef): SubscriptionState = {
-      val wasSubscribed = subscribed.getOrElse(actor, Set.empty)
-      val newSubscribed = subscribed - actor
-      val newSubscriber = wasSubscribed.foldLeft(subscriber) { (s, channel) =>
-        val x = s.getOrElse(channel, Set.empty) - actor
-        if (x.isEmpty) s - channel else s.updated(channel, x)
-      }
-      SubscriptionState(subscriber = newSubscriber, subscribed = newSubscribed)
-    }
-
     def numSubscriber(channel: String): Int = {
       subscriber.getOrElse(channel, Set.empty).size
     }
   }
 
-}
-
-class RedisSubscriberActor(redisClient: RedisClient)
-  extends Actor
-    with ActorLogging {
-
-  import RedisSubscriber._
-
-  var state = SubscriptionState(subscriber = Map.empty, subscribed = Map.empty)
-
-  override def receive: Receive = {
-    // from other actor
-    case Subscribe(channels) =>
-      state = state.onSubscribe(sender, channels: _*)
-      val me = self
-      redisClient.subscribe(channels.head, channels.tail: _*) {
-        msg: PubSubMessage =>
-          // from com.redis.PubSub.Consumer thread
-          me ! msg
-      }
-
-    case Unsubscribe(channels) =>
-      val oldState = state
-      state = state.onUnsubscribe(sender, channels: _*)
-      val toUnsubscribe = channels.filter(
-        c => oldState.numSubscriber(c) > 0 && state.numSubscriber(c) == 0
-      )
-      if (toUnsubscribe.nonEmpty) {
-        redisClient.unsubscribe(toUnsubscribe.head, toUnsubscribe.tail: _*)
-      }
-
-    case UnsubscribeAll() =>
-      val wasSubscribing = state.subscribed.getOrElse(sender, Set.empty).toSeq
-      state = state.onUnsubscribeAll(sender)
-      val toUnsubscribe =
-        wasSubscribing.filter(c => state.numSubscriber(c) == 0)
-      if (toUnsubscribe.nonEmpty) {
-        redisClient.unsubscribe(toUnsubscribe.head, toUnsubscribe.tail: _*)
-      }
-
-    // from com.redis
-    case S(channel, count) =>
-      log.info("subscribed to {} / subscribing to {}", channel, count)
-
-    case U(channel, count) =>
-      log.info("unsubscribed to {} / subscribing to {}", channel, count)
-
-    case M(channel, data) =>
-      log.info("message has arrived from redis.")
-
-    // NOT handling com.redis.redisclient.E
-  }
 }
